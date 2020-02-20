df7750efdc9c3f63e6b0efac049e1e75e080dc61
michaeld55 <michaeld55@hotmail.co.uk>
2020-02-20 14:31:00 +0000

    added food tests

--- starting_point/friends.rb
+++ starting_point/friends.rb
@@ -1,3 +1,5 @@
+require("pry-byebug")
+
 def get_name(person)
   return person[:name]
 end
@@ -5,5 +7,13 @@ end
 def get_tv_show(person)
 
   return person[:favourites][:tv_show]
+
+end
+
+def likes_to_eat(person, food)
+
+  any_person = person[:favourites][:snacks].any?
+
+   return any_person
 
 end
